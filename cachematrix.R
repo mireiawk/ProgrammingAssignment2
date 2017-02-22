422aadaed9237e7fc7828529c62a06fee74d5665
mireiawk <mireia.wk@gmail.com>
2017-02-22 10:42:35 +0100

    adding comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow to cache an inverse of 
+## a matrix to reduce time of calculations for the
+## same matrix.
 
-## Write a short comment describing this function
+## This function takes a matrix as input and make 
+## an object to be able to caching input.
+## inv variable is used to store caching input so
+## is reset whenever the matrix is changed.
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
@@ -18,7 +22,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function return the inverse of a matrix
+## but always try to get a cached version if exists.
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
